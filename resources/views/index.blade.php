35ce500c6263e56739de1dda04a803a50e5fb470
screendragons <chiyuyeung@outlook.com>
2019-05-20 22:03:40 +0200

    Update profile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
           <meta name="description" content="">
           <meta name="author" content="">
 
-          <title>DeviantArtu</title>
+          <title>DeviantArt</title>
 
           <!-- Font Awesome Icons -->
           <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
